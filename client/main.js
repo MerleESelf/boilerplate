e10c457aa9c22f76cfc58aef3daa668ed1af31b3
Merle <merleself@gmail.com>
2022-02-24 18:51:12 -0500

    nav and routes in render

--- client/main.js
+++ client/main.js
@@ -3,10 +3,14 @@ import React from "react";
 import ReactDOM from 'react-dom' 
 import { Provider } from 'react-redux'
 import store from './store/index'
+import Navbar from './components/Navbar'
+import Routes from './components/Routes'
  
 ReactDOM.render(
     <Provider store={store}>
     <div> 
+        <Navbar /> 
+        <Routes /> 
     </div>
     </Provider>, 
     document.getElementById('app')
